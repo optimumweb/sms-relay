bb1170751cbe769ee94d82b7263cb9822a5702c8
Jonathan Roy <jroy@optimumweb.ca>
2017-06-25 23:26:21 -0400

    dev

--- send.php
+++ send.php
@@ -11,8 +11,6 @@ if ( $sock = fopen("php://stdin", 'r') ) {
         $stdin .= fread($sock, 1024);
     }
     fclose($sock);
-} else {
-    echo 'no stdin' . PHP_EOL;
 }
 
 if ( !empty($stdin) ) {
@@ -25,50 +23,15 @@ if ( !empty($stdin) ) {
 
             if ( !empty($email->from) && $email_domain = @explode('@', $email->from, 2)[1] ) {
 
-                if ( defined('AUTHORIZED_DOMAIN') ) {
+                if ( defined('AUTHORIZED_DOMAIN') && $email_domain == AUTHORIZED_DOMAIN ) {
 
-                    if ( $email_domain == AUTHORIZED_DOMAIN ) {
+                    $authorization_code = $email->subject;
+                    $tel = @explode('@', $email->to, 2)[0];
 
-                        if ( defined('AUTHORIZATION_CODE') ) {
+                    $body = $email->plain();
 
-                            if ( !empty($email->subject) && $email->subject == AUTHORIZATION_CODE ) {
-
-                                if ( !empty($email->to) && $tel = @explode('@', $email->to, 2)[0] ) {
-
-                                    if ( strpos($tel, '+1') === false ) {
-                                        $tel = '+1' . $tel;
-                                    }
-
-                                    $body = $email->plain();
-
-                                    if ( !empty($body) ) {
-
-                                        if ( strpos($body, '---') !== false ) {
-                                            $body = explode('---', $body)[0];
-                                        }
-
-                                        if ( !empty($body) ) {
-
-                                            try {
-
-                                                $twilio_client = new Twilio\Rest\Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-
-                                                $message = $twilio_client->messages->create($tel, [ 'from' => TWILIO_SMS_FROM, 'body' => $body ] );
-
-                                            } catch ( Exception $e ) {
-
-                                            }
-
-                                        }
-
-                                    }
-
-                                }
-
-                            }
-
-                        }
-
+                    if ( !empty($body) && strpos($body, '---') !== false ) {
+                        $body = explode('---', $body)[0];
                     }
 
                 }
@@ -78,6 +41,32 @@ if ( !empty($stdin) ) {
         }
 
     } catch ( Exception $e ) {
+
+    }
+
+} elseif ( !empty($_GET['tel']) && !empty($_GET['body']) && !empty($_GET['authorization_code']) ) {
+    $tel = $_GET['tel'];
+    $body = $_GET['body'];
+    $authorization_code = $_GET['authorization_code'];
+}
+
+if ( !empty($authorization_code) && $authorization_code == AUTHORIZATION_CODE ) {
+
+    if ( !empty($tel) && !empty($body) ) {
+
+        if ( strpos($tel, '+1') === false ) {
+            $tel = '+1' . $tel;
+        }
+
+        try {
+
+            $twilio_client = new Twilio\Rest\Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+            $message = $twilio_client->messages->create($tel, [ 'from' => TWILIO_SMS_FROM, 'body' => $body ] );
+
+        } catch ( Exception $e ) {
+
+        }
 
     }
 

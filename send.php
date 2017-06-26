a410e9669c2f8e86b250f5ba707c74f8342b38e1
Jonathan Roy <jroy@optimumweb.ca>
2017-06-26 15:01:38 -0400

    dev

--- send.php
+++ send.php
@@ -5,51 +5,37 @@ define('ABS_PATH', dirname(__FILE__));
 
 require_once ABS_PATH . '/inc/init.php';
 
-if ( $sock = fopen("php://stdin", 'r') ) {
-    $stdin = '';
-    while ( !feof($sock) ) {
-        $stdin .= fread($sock, 1024);
-    }
-    fclose($sock);
-}
 
-if ( !empty($stdin) ) {
+if ( $sock = fopen('php://stdin', 'r') ) {
 
     try {
 
-        $email_parser = new Email_Parser;
+        $email_parser = new PhpMimeMailParser\Parser;
 
-        if ( $email = $email_parser->parse($stdin) ) {
+        $email_parser->setStream($sock);
 
-            $email_from = parse_email_address($email->from);
-            $email_to   = parse_email_address($email->to);
+        $email_from = parse_email_address($email_parser->getHeader('from'));
+        $email_to   = parse_email_address($email_parser->getHeader('to'));
 
-            $tel  = @explode('@', $email_to, 2)[0];
-            $body = @explode('---', $email->plain())[0];
+        $tel  = @explode('@', $email_to, 2)[0];
+        $body = @explode('---', $email_parser->getMessageBody('text'))[0];
 
-            if ( !empty($email_from) && $email_domain = @explode('@', $email_from, 2)[1] ) {
-                if ( $email_domain == AUTHORIZED_DOMAIN ) {
-                    $authorization_code = $email->subject;
-                }
+        if ( !empty($email_from) && $email_domain = @explode('@', $email_from, 2)[1] ) {
+            if ( $email_domain == AUTHORIZED_DOMAIN ) {
+                $authorization_code = $email_parser->getHeader('subject');
             }
-
-            app_log('stdin: ' . $stdin);
-            app_log('email_from: ' . $email_from);
-            app_log('email_to: ' . $email_to);
-            app_log('tel: ' . $tel);
-            app_log('body: ' . $body);
-            app_log('authorization_code: ' . $authorization_code);
-
         }
 
+        app_log('email_from: ' . $email_from);
+        app_log('email_to: ' . $email_to);
+        app_log('tel: ' . $tel);
+        app_log('body: ' . $body);
+        app_log('authorization_code: ' . $authorization_code);
+
     } catch ( Exception $e ) {
-        app_log("Email_Parser exception thrown: " . $e);
+        app_log($e);
     }
 
-} elseif ( !empty($_GET['tel']) && !empty($_GET['body']) && !empty($_GET['authorization_code']) ) {
-    $tel = $_GET['tel'];
-    $body = $_GET['body'];
-    $authorization_code = $_GET['authorization_code'];
 } else {
     app_log("No data supplied!");
 }

428c5dda50ccccac4f1012de90bfecff58802afe
Jonathan Roy <jroy@optimumweb.ca>
2017-06-26 15:51:06 -0400

    dev

--- receive.php
+++ receive.php
@@ -20,13 +20,13 @@ if ( !empty($_POST) ) {
         $headers    = sprintf("From: %s\r\nReply-To: %s\r\nX-Mailer: PHP/%s", $email_from, $email_from, phpversion());
 
         if ( mail($email_to, $subject, $body, $headers) ) {
-            app_log("SMS message from '%s' relayed successfully to '%s'", $from, $email_to);
+            app_log(sprintf("SMS message from '%s' relayed successfully to '%s'", $from, $email_to));
         } else {
-            app_log("SMS message from '%s' could not be relayed to '%s'", $from, $email_to);
+            app_log(sprintf("SMS message from '%s' could not be relayed to '%s'", $from, $email_to));
         }
 
     } else {
-        app_log("No previous SMS message to '%s'", $from);
+        app_log(sprintf("No previous SMS message to '%s'", $from));
     }
 
 }

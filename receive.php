09e8717a31e93ce14a55ca333d5fa1547b92d14d
Jonathan Roy <jroy@optimumweb.ca>
2017-06-26 09:48:25 -0400

    add receiving capabilities

--- receive.php
+++ receive.php
@@ -4,5 +4,5 @@ define('ABS_PATH', dirname(__FILE__));
 
 require_once ABS_PATH . '/inc/init.php';
 
-app_log('GET: ' . $_GET);
-app_log('POST: ' . $_POST);
+app_log('GET: ' . var_export($_GET, true));
+app_log('POST: ' . var_export($_POST, true));

d0f760b16df687c57a44e25165cefdaaca2e474e
Jonathan Roy <jroy66@gmail.com>
2022-01-23 11:47:09 -0500

    debug

--- inc/init.php
+++ inc/init.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+
 require_once ABS_PATH . '/vendor/autoload.php';
 
 require_once ABS_PATH . '/inc/config.php';
